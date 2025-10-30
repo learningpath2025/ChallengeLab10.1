3cbdbfe09559dd0da9f6a95895569c47e30da6fc
learningpath2025 <learningpath2025@outlook.com>
2025-10-30 08:53:54 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ChallengeLab10._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Letter that was added to the second string is { FindDifference("abcd", "abcde")}.");
+            Console.ReadLine();
+        }
+
+        public static string FindDifference(string s, string t)
+        {
+            var d = "";
+            if (s != null && t != null)
+            {
+                d = t.Except(s).FirstOrDefault().ToString();
+            }
+            return d;
+        }
+    }
+}
